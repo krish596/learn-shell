4651b6a7d847ed1b38795913178037c521f60cf2
Chimtamreddy <kr7348202@gmail.com>
2024-08-02 20:57:42 +0530

    roboshop-shellscript

---
+++ 01-print.sh
@@ -0,0 +1,17 @@
+# Print
+
+# Red
+# Green
+# Blue
+# Yellow
+# Orange
+# Magenta
+
+# echo -e "\e[31mCodeMsg\e[0m"
+
+echo -e "\e[31mHelloWorld\e[0m"
+echo -e "\e[32mHelloWorld\e[0m"
+echo -e "\e[33mHelloWorld\e[0m"
+echo -e "\e[34mHelloWorld\e[0m"
+echo -e "\e[35mHelloWorld\e[0m"
+echo -e "\e[36mHelloWorld\e[0m"
