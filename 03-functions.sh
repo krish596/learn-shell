fcbb35084b8aa3a2b37a0038a65950ad76081678
Chimtamreddy <kr7348202@gmail.com>
2024-08-03 15:35:42 +0530

    roboshop-shellscript

--- 03-functions.sh
+++ 03-functions.sh
@@ -7,4 +7,15 @@ greeting() {
 
 greeting
 
+echo exit status - $0
+
+inner() {
+  echo hello, good morning
+  echo welcome to devops training
+  return 10
+  echo have you here
+}
+
+inner
+
 echo exit status -
